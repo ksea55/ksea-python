04d2f922b4848d6d633e71c466c2561969843c49
ksea55 <wangyang198118@126.com>
2017-09-19 08:58:02 +0800

    if elif else

---
+++ py/chapter3/if.py
@@ -0,0 +1,19 @@
+# 在python中控制语句与java中区别不大，其主要关键字 if  elif  else
+
+
+if True:
+    print("this is True")  # this is True
+
+num = int(input("请输入你要输入的数字:"))
+
+if num < 5:
+    print("在5的左边!")
+elif num > 5:
+    print("在5的右边!")
+else:
+    print("哈哈,它就是它")
+print("俺enter退出!")
+
+
+
+
